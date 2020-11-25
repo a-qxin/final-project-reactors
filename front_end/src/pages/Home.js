5f5a6019fd9aad83c4d8716bbab3c2bef13e2854
aqxin <aqxin.cs@gmail.com>
2020-11-25 11:26:51 -0800

    Add a sample Listing.js

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 // import { useSelector } from 'react-redux';
 
+import Listing from './Listing.js';
+
 const Home = () => {
   return (
     <div>
       <div>
         {/* <h1>reactorsHub</h1> */}
         <div>
-          welcome
+          <h3>welcome</h3>
+        </div>
+
+        <div>
+          <Listing />
         </div>
       </div>
     </div>
---
+++ front_end/src/pages/Listing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import defaultImage from '../assets/defaultimage.png';
+
+const Listing = () => {
+  const listingContainer = {
+    textAlign:'center', 
+    backgroundColor:'rgba(255, 255, 255, 0.4)', 
+    // margin:'auto', 
+    width:'300px', 
+    padding:'50px 0 20px 0', 
+    borderRadius:'30px'
+  };
+
+  return (
+    <div>
+      <div>
+        <div style={listingContainer}>
+          <img src={defaultImage} width="120px" alt='defaultImage'/>
+          <h4>Smol Reactor</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Listing;
